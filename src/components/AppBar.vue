ffc85199e01293db4d1732e3ba4517f47059f992
JohannesPlath <plath.johannes@gmail.com>
2022-11-24 18:21:12 +0100

    changeAppBar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -5,7 +5,7 @@
   >
     <template #image>
       <v-img
-        gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+        gradient="to top right,to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
       />
     </template>
 
