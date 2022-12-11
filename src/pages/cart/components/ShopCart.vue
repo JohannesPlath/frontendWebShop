5cbe2cf0a91ae4c0cbf4b0a6451577d8a0c71b2a
JohannesPlath <plath.johannes@gmail.com>
2022-12-11 18:34:18 +0100

    first cart steps

--- src/pages/cart/components/ShopCart.vue
+++ src/pages/cart/components/ShopCart.vue
@@ -4,25 +4,26 @@
     max-width="600"
   >
     <v-toolbar color="secondary">
-      <v-btn variant="text" icon="mdi-menu"></v-btn>
 
-      <v-toolbar-title>My files</v-toolbar-title>
+
+      <v-toolbar-title>Your Choose:</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn variant="text" icon="mdi-magnify"></v-btn>
+<!--      <v-btn variant="text" icon="mdi-magnify"></v-btn>
 
-      <v-btn variant="text" icon="mdi-view-module"></v-btn>
+      <v-btn variant="text" icon="mdi-view-module"></v-btn>-->
     </v-toolbar>
 
     <v-list lines="two">
       <v-list-subheader inset>Cart:</v-list-subheader>
 
       <v-list-item
-        v-for="product in cartProducts"
-        :key="product.uuid"
-        :title="product.title"
-        :subtitle="product.count"
+        v-for="item in cartProducts"
+        :key="item.product.uuid"
+        :title="item.product.title"
+        :subtitle="'count: ' + item.product.count + ', price: ' + item.product.price +' = '+ item.product.count * item.product.price + item.product.currency"
+
       >
         <template v-slot:prepend>
           <v-avatar color="grey-lighten-1">
